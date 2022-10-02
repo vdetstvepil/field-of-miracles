0f5f04bd299caf8228f1c2dff21aa8f7177c578c
ZUSHI <shiganov.e@rambler.ru>
2022-10-02 18:27:10 +0300

    Added class StatisticsPageViewModel

---
+++ ClientApp/ViewModel/Pages/StatisticsPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApp.Model;
+using ClientApp.Services;
+
+namespace ClientApp.ViewModel.Pages
+{
+    public class StatisticsPageViewModel : Model.ViewModel
+    {
+        ObservableCollection<StatisticsItem> _statisticsItems;
+
+        public static ObservableCollection<StatisticsItem> UpdateStatistics(string fileName)
+        {
+            SQLiteConnection connection = new SQLiteConnection($"Data Source={fileName}; Version=3;");
+
+            // Выясняем колво строк
+            int count = DatabaseHandler.SelectQuery(ref connection, "statistics", "id").Count;
+
+            ObservableCollection<StatisticsItem> list = new ObservableCollection<StatisticsItem>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = DatabaseHandler.SelectQuery(ref connection, "statistics", "name", $"id == {i}")[0].ToString();
+                int sum = (int)DatabaseHandler.SelectQuery(ref connection, "statistics", "name", $"id == {i}")[0];
+                StatisticsItem item = new StatisticsItem(name, sum);
+                list.Add(item);
+            }
+
+
+
+            return null;
+        }
+    }
+}
